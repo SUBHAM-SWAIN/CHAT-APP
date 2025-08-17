761988dd036aee7eede7a78fd0f3328eb8d035ec
SUBHAM SWAIN <subhamswain277@gmail.com>
2025-08-17 23:09:08 +0530

    complete

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.Frontend_URL,
+    origin: "http://localhost:5173" || process.env.Frontend_URL ,
     credentials: true,
   })
 );
