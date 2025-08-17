b75b112b58662a4280d3875d09a0661f32c40991
SUBHAM SWAIN <subhamswain277@gmail.com>
2025-08-17 23:09:46 +0530

    complete

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstans = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL:"http://localhost:5001/api" || import.meta.env.BACKEND_URL/api, 
   withCredentials:true
 })
