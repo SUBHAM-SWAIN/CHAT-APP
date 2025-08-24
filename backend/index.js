67649c0550ea8b515f129efef9198b825c98a5c7
SUBHAM SWAIN <subhamswain277@gmail.com>
2025-08-24 14:14:49 +0530

    Ready to deploy

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin:["http://localhost:5173", "https://chat-app-xi-neon.vercel.app"],
     credentials: true,
   })
 );
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const axiosInstans = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chat-app-takw.onrender.com/api",
 
   withCredentials:true
 })
